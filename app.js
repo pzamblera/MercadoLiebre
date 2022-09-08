77d9d22a84f4589b81b872a08e92c0c62b9709f8
pzamblera <pzamblera@gmail.com>
2022-09-08 12:41:41 -0300

    16

--- app.js
+++ app.js
@@ -5,17 +5,20 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.get("/", function(req, res) => {
-    res.sendFile(path.join(__dirname, './views/home.html'));
+app.get("/", function(req, res){
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
 
-app.get("/register", function(req, res) => {
-    res.sendFile(path.join(__dirname, "./views/register.html"));
+app.get("/register", function(req, res){
+    res.sendFile(path.resolve(__dirname, "./views/register.html"));
 })
 
-app.get("/login", function(req, res) => {
-    res.sendFile(path.join(__dirname, "./views/login.html"));
+app.get("/login", function(req, res){
+    res.sendFile(path.resolve(__dirname, "./views/login.html"));
 })
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, function(){
     console.log("Servidor corriendo");
+})
+app.get("*", function(req, res){
+    res.send("Ruta restringida");
 })
