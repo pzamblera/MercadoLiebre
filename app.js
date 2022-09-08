315e2106061c30098c5f26e36f1d28e4e509517a
pzamblera <pzamblera@gmail.com>
2022-09-08 07:37:26 -0300

    12

--- app.js
+++ app.js
@@ -3,18 +3,18 @@ const app = express();
 
 const path = require("path");
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, /public')));
 
 app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, './views/index.html'));
+    res.sendFile(path.join(__dirname, "/views/index.html"));
 });
 
 app.get("/register", (req, res) => {
-    res.sendFile(path.join(__dirname, "./views/register.html"));
+    res.sendFile(path.join(__dirname, "/views/register.html"));
 })
 
 app.get("/login", (req, res) => {
-    res.sendFile(path.join(__dirname, "./views/login.html"));
+    res.sendFile(path.join(__dirname, "/views/login.html"));
 })
 app.listen(process.env.PORT || 3000, function() {
     console.log("Servidor corriendo");
