36cfa2cbb8f3f5c5ccc0a0d78887e5fa3455f106
pzamblera <pzamblera@gmail.com>
2022-09-07 23:08:57 -0300

    v6

--- app.js
+++ app.js
@@ -17,8 +17,6 @@ app.get("/login", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/login.html"));
 })
 
-app.listen(3000, () => console.log ("servidor correido en puerto 3000"));
-
 app.listen(process.env.PORT || 3000, function(){
     console.log("servidor correido en puerto 3000");
 })
