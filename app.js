5d41c6a7a52eef9f0931d746a820381ca9e707ac
pzamblera <pzamblera@gmail.com>
2022-09-08 12:45:02 -0300

    17

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get("/", function(req, res){
-    res.sendFile(path.resolve(__dirname, './views/home.html'));
+    res.sendFile(path.resolve(__dirname, '/views/home.html'));
 });
 
 app.get("/register", function(req, res){
-    res.sendFile(path.resolve(__dirname, "./views/register.html"));
+    res.sendFile(path.resolve(__dirname, "/views/register.html"));
 })
 
 app.get("/login", function(req, res){
-    res.sendFile(path.resolve(__dirname, "./views/login.html"));
+    res.sendFile(path.resolve(__dirname, "/views/login.html"));
 })
 app.listen(process.env.PORT || 3000, function(){
     console.log("Servidor corriendo");
