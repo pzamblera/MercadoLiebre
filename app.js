367313d38d4d190643af4fec007a1ca7e93c0875
pzamblera <pzamblera@gmail.com>
2022-09-07 23:11:10 -0300

    v7

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ const path = require("path");
 app.use(express.static(path.resolve(__dirname, "./public")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/home.html"));
+    res.sendFile(path.resolve(__dirname, "/views/home.html"));
 })
 
 app.get("/register", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/register.html"));
+    res.sendFile(path.resolve(__dirname, "/views/register.html"));
 })
 
 app.get("/login", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/login.html"));
+    res.sendFile(path.resolve(__dirname, "/views/login.html"));
 })
 
 app.listen(process.env.PORT || 3000, function(){
