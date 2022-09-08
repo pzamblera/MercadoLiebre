6d764425854152d9527ed8307968b81d6cb3cd3e
pzamblera <pzamblera@gmail.com>
2022-09-08 07:40:24 -0300

    12

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, /public')));
 
 app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "/views/index.html"));
+    res.sendFile(path.join(__dirname, "/views/home.html"));
 });
 
 app.get("/register", (req, res) => {
